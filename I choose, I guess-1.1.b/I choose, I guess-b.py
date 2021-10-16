0e201bebbb425fb847db6ecb20db04da304842f2
AmirHossein <86773484+AaMmIiRrHhOoSsEeIiNn@users.noreply.github.com>
2021-10-16 20:16:45 +0330

    Update I choose, I guess-b.py

--- I choose, I guess-1.1.b/I choose, I guess-b.py
+++ I choose, I guess-1.1.b/I choose, I guess-b.py
@@ -14,14 +14,14 @@ guess_list_min = []
 guess_list_max = []
 range_list = []  # end_range_o save for each n_data in this; 
 guess_list = []  # Number of guess to find the RandomNumber for each repeatition;
-n_data = 3  # n_data: Number of MAX/MIN geusses (n_data for MAX and n_data for MIN) (DETERMINE)
+n_data = 3  # n_data: Number of MAX/MIN geusses (n_data for MAX and n_data for MIN) (DETERMINE THIS)
 end_range_o = 0  # End of range for start
 base_repeat = 0  
 
 for  data in range(n_data): # Total data (in this loop we add step range(end_range_o) and and step repeatirion(base_repaet) to last ones;
     print("Data pack: "+str(data+1))
-    base_repeat += 5   # In each n_data we add base_repeat more to repeats variables. (DETERMINE)
-    end_range_o += 100  # For each n_data, range increase as much as this.(DETERMINE)
+    base_repeat += 5   # In each n_data we add base_repeat more to repeats variables. (DETERMINE THIS)
+    end_range_o += 100  # For each n_data, range increase as much as this.(DETERMINE THIS)
     range_list.append(end_range_o)  # Save range of each repeatition.
 
     for repeat in range(base_repeat): # Repeat loop (this loop choose a random number (range is canstant))
